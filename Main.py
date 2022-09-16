ccbcd618b483aee03f45ed68debe9ece244609df
Goniix <dragonixgold@gmail.com>
2022-09-16 07:28:59 +0200

    Added monster.py file Added speed stats to instances renamed bad named vars

--- Main.py
+++ Main.py
@@ -19,32 +19,42 @@ class Draw:
 # game instances
 class Player:
     def __init__(self):
-        self.baseHealth = 500
-        self.Health = self.baseHealth
         self.coord = [0, 0]
         self.inv = []
+        self.base_health = 500
+        self.health = self.base_health
+        self.base_speed = 5
+        self.speed = self.base_speed
 
     def move_player(self, x_direction, y_direction):
         pass
 
 
 class Item:
-    def __init__(self, x, y, itemBase):
+    def __init__(self, x, y, item_base):
         self.coord = [x, y]
-        self.stepMethod = itemBase.method
-        self.description = itemBase.description
-        self.type = itemBase.type
+        self.collide_method = item_base.collide_method
+        self.step_method = item_base.step_method
+        self.description = item_base.description
+        self.type = item_base.type
+
+    def collide(self):
+        self.collide_method()
 
     def execute_item(self):
-        self.stepMethod()
+        self.step_method()
 
 
 class Monster:
-    def __init__(self, x, y, monsterBase):
+    def __init__(self, x, y, monster_base):
         self.coord = [x, y]
-        self.stepMethod = monsterBase.method
-        self.description = monsterBase.description
-        self.type = monsterBase.type
+        self.step_method = monster_base.method
+        self.description = monster_base.description
+        self.type = monster_base.type
+        self.base_health = monster_base.baseHealth
+        self.health = self.base_health
+        self.base_speed = monster_base.baseSpeed
+        self.speed = self.base_speed
 
 
 game = Game()
---
+++ monster.py
@@ -0,0 +1,1 @@
+# store monster_base here
