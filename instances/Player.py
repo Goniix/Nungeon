c4120f294aa67cd63659ccd22475f85fe6cbe1a8
Goniix <87310572+Goniix@users.noreply.github.com>
2022-09-16 11:26:35 +0200

    Fixed player not having name

--- instances/Player.py
+++ instances/Player.py
@@ -1,11 +1,12 @@
 class Player:
-    def __init__(self):
+    def __init__(self,name):
         self.coord = [0, 0]
         self.inv = []
         self.base_health = 500
         self.health = self.base_health
         self.base_speed = 5
         self.speed = self.base_speed
+        self.name = name
 
     def move_player(self, x_direction, y_direction):
         pass
