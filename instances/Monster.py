62d6c237555505ae068b3515e36b9c5f0024f640
Goniix <87310572+Goniix@users.noreply.github.com>
2022-09-16 12:04:36 +0200

    Added stats to monster class and method storage (#15)

--- instances/Monster.py
+++ instances/Monster.py
@@ -1,10 +1,29 @@
 class Monster:
     def __init__(self, x, y, monster_base):
         self.coord = [x, y]
-        self.step_method = monster_base.method
-        self.description = monster_base.description
         self.type = monster_base.type
         self.base_health = monster_base.baseHealth
         self.health = self.base_health
         self.base_speed = monster_base.baseSpeed
         self.speed = self.base_speed
+        self.speedgen = monster_base.speedgen
+        self.description = monster_base.description
+        self.name = monster_base.name
+
+        self.step_method = monster_base.step_method
+        self.attack_method = monster_base.attack_method
+        self.hit_method = monster_base.hit_method
+        self.death_method = monster_base.death_method
+
+class MonsterTemplate:
+    def __init__(self) -> None:
+        self.name = "Defaultname"
+        self.description = "DefaultDescription"
+        self.base_health = 500
+        self.base_speed = 10
+        self.speedgen = 10
+        self.type = "M"
+
+    def step_method(self):
+        """method executed each turn by the monster"""
+        pass
