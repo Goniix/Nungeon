14acd7aae7a336c29a38a8fe19dcc994f7b5e1f4
Goniix <87310572+Goniix@users.noreply.github.com>
2022-09-16 11:32:52 +0200

    Added player cordinate in init param and deleted useess speed var in player (#11)

--- instances/Player.py
+++ instances/Player.py
@@ -1,12 +1,10 @@
 class Player:
-    def __init__(self,name):
-        self.coord = [0, 0]
+    def __init__(self,name, coords):
+        self.coord = [coords[0], coords[1]]
         self.inv = []
         self.base_health = 500
         self.health = self.base_health
-        self.base_speed = 5
-        self.speed = self.base_speed
-        self.name = name
+        self.player_name = name
 
     def move_player(self, x_direction, y_direction):
         pass
