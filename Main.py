1bc12dd588615a4cc171e0d5a092465892a84d42
pioupia <49518790+pioupia@users.noreply.github.com>
2022-09-16 11:19:23 +0200

    Feat: Create Game Class and Colors class

--- Main.py
+++ Main.py
@@ -1,25 +1,42 @@
-from .instances.Item import Item
+from instances.Item import Item
+from instances.Colors import Colors
+from instances.Item import *
+from instances.Monster import *
+from instances.Player import Player
 
 class Game:
     def __init__(self):
-        self.run = True
+        self.colors = Colors()
+        self.player = None
+
+        self.run = False
+
+    
+    def ask_player_name(self) -> int:
+        if self.player is not None:
+            print("Un joueur est déjà créé.")
+            return 1
+
+        player_name = input("Veuillez renseigner le pseudonyme du joueur : ")
+        
+        self.player = Player(player_name, (0, 0))
+
+        return 0
 
     def update(self):
         # run updates of all instances (no draw)
         pass
 
 
-class Draw:
-    def __init__(self):
-        pass
+    def start_game(self) -> int:
+        self.ask_player_name()
 
-    def update(self):
-        # update display
-        pass
+        self.run = True
 
+        while self.run:
+            self.update()
+
+        return 0
 
 game = Game()
-display = Draw()
-while game.run:
-    game.update()
-    display.update()
+game.start_game()
