cd3aa1b4a87e571d685372e6c6662e25de656834
Goniix <87310572+Goniix@users.noreply.github.com>
2022-09-23 10:32:46 +0200

    finished ia movement

--- instances/Monster.py
+++ instances/Monster.py
@@ -22,12 +22,16 @@ class MonsterTemplate:
         self.base_health = 500
         self.base_speed = 10
         self.type = "M"
+        self.base_speed = 10
+        self.speed = self.base_speed
 
-    def step_method(self,player_x,player_y):
+    def step_method(self,player_x,player_y,tcor):
         """method executed each turn by the monster"""
         dist=sqrt(((player_x-self.coord[0])**2)-((player_y-self.coord[1])**2))
-        self.coord[0]+= 0
-        pass
+        x_vec=(player_x-self.coord[0])/dist
+        y_vec=(player_y-self.coord[1])/dist
+        self.coord[0]+= x_vec*self.speed*tcor
+        self.coord[1]+= y_vec*self.speed*tcor
 
     def death_method(self):
         pass
