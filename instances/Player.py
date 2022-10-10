9abd9b37600da995caf1847926669ad8ac91a93b
Goniix <dragonixgold@gmail.com>
2022-10-10 23:05:09 +0200

    fixed add_item_to_inv

--- instances/Player.py
+++ instances/Player.py
@@ -27,7 +27,7 @@ class Player:
         return self.inv
     
     def add_item_to_inv(self,itm):
-        inv.append(itm)
+        self.inv.append(itm)
     
     def move_player(self, tcor, x_direction, y_direction):
         self.coord[0]+= x_direction*self.speed*tcor
