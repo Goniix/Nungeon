114e58233e81827fcde17f12c326a549e2c2a642
Goniix <87310572+Goniix@users.noreply.github.com>
2022-10-10 15:06:38 +0200

    Add files via upload

--- instances/Player.py
+++ instances/Player.py
@@ -1,18 +1,41 @@
+import pygame
 class Player:
-    def __init__(self,name, coords, sprite):
+    def __init__(self,name, coords, sprt, screen):
         self.coord = [coords[0], coords[1]]
         self.inv = []
         self.base_health = 500
         self.health = self.base_health
         self.player_name = name
-        self.base_speed = 10
+        self.base_speed = 25
         self.speed = self.base_speed
-        self.sprite = sprite
-
+        self.sprt = sprt
+        self.scrn = screen
+        
+    def get_x(self):
+        return self.coord[0]
+    
+    def set_x(self,x):
+        self.coord[0] = x
+        
+    def get_y(self):
+        return self.coord[1]
+    
+    def set_y(self,y):
+        self.coord[1] = y
+        
+    def get_inv(self):
+        return self.inv
+    
+    def add_item_to_inv(self,itm):
+        inv.append(itm)
+    
     def move_player(self, tcor, x_direction, y_direction):
         self.coord[0]+= x_direction*self.speed*tcor
         self.coord[1]+= y_direction*self.speed*tcor
     
     def draw(self):
-        pass
-        #draw at players coord
+        self.scrn.blit(self.sprt,[1194/2,834/2])
+    
+    def update(self, dt, keyinput):
+        self.set_x(self.get_x()+(keyinput[0]*self.speed*dt))
+        self.set_y(self.get_y()+(keyinput[1]*self.speed*dt))
