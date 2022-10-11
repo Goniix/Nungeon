f94f1962986147ed035f3c620b2c618e17b99647
Goniix <dragonixgold@gmail.com>
2022-10-11 14:47:13 +0200

    taille des background tiles encore augmentée
    -taille du sprite 64->512

--- Main.py
+++ Main.py
@@ -29,14 +29,14 @@ class Game:
         self.colors = Colors()
         self.player = None
         
-        self.fps_cap = 60
+        self.fps_cap = 160
 
         self.run = False
 
 
         self.sprites = {}
         self.sprites["player"] = pygame.image.load(os.path.join("sprites/player.png")).convert()
-        self.sprites["background"] = pygame.image.load(os.path.join("sprites/background_tile_2X.png")).convert()
+        self.sprites["background"] = pygame.image.load(os.path.join("sprites/background_tile.png")).convert()
         self.my_font = pygame.font.SysFont('Comic Sans MS', 10)
 
         self.debug=tools.devtools.Debug()
@@ -74,18 +74,18 @@ class Game:
     def draw(self):
         screen.fill((0, 0, 0))
         
-        count_per_line = floor(width / 128) + 2
-        count_per_column = floor(height / 128) + 2
+        count_per_line = floor(width / 512) + 2
+        count_per_column = floor(height / 512) + 2
 
-        max_decal_x = floor(self.player.coord[0] % 128)
-        max_decal_y = floor(self.player.coord[1] % 128)
+        max_decal_x = floor(self.player.coord[0] % 512)
+        max_decal_y = floor(self.player.coord[1] % 512)
 
         
         temp=[]
         
         for i in range(count_per_line):
             for j in range(count_per_column):
-                temp.append((self.sprites["background"], [(i * 128) - max_decal_x, (j * 128) - max_decal_y]))
+                temp.append((self.sprites["background"], [(i * 512) - max_decal_x, (j * 512) - max_decal_y]))
                 
         
         temp+=self.debug.draw()
